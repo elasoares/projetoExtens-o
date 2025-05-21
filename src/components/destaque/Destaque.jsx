872c8ea004f225046b50f047ba2f5f476cdd3399
Elaine Soares <131168713+elasoares@users.noreply.github.com>
2025-05-20 23:00:14 -0300

    Enviando para o remoto atualização feita nos cards do destaque

--- src/components/destaque/Destaque.jsx
+++ src/components/destaque/Destaque.jsx
@@ -3,12 +3,14 @@ import img1 from "../../assets/1.jpg"
 import img2 from "../../assets/2.jpg"
 import img3 from "../../assets/3.jpg"
 import img4 from "../../assets/4.jpg"
+import { CiShoppingCart } from "react-icons/ci";
 
 const destaque = [
-    {img: img1, title:"Bermuda com elas...", brand:"nike", price:"R$ 00,00"},
-    {img: img2, title:"Camisetas", brand:"EA", price:"R$ 00,00"},
-    {img: img3, title:"Camisetas", brand:"nike", price:"R$ 00,00"},
-    {img: img4, title:"Camisetas", brand:"Boss", price:"R$ 00,00"},
+    {img: img1, title:"Bermuda com elas...", brand:"nike"},
+    {img: img2, title:"Camisetas", brand:"EA"},
+    {img: img3, title:"Camisetas", brand:"nike"},
+    {img: img4, title:"Camisetas", brand:"Boss"},
+
 ]
 
 export default function Destaque(){
@@ -26,7 +28,7 @@ export default function Destaque(){
                                 <h3 className={styles.description}>{item.title}</h3>
                                 <p className={styles.brand}>{item.brand}</p>
                             </div>
-                            <button className={styles.price}>{item.price}</button>
+                            <a className={styles.cart} href=''><CiShoppingCart /></a>
                         </div>
                     </div>
                 ))}
